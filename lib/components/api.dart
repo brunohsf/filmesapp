e181140652c96088ff32ffa66dde4134c59984a3
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-08-26 19:37:31 -0300

    implementa api.dart

---
+++ lib/components/api.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_initializing_formals
+import 'package:http/http.dart' as http;
+
+// A URL da API
+const baseUrl = "https://api.tvmaze.com/search/shows?q=";
+
+// Criamos a classe da nossa API. O nome você que escolhe. Fazemos aqui
+// uma requisição get (como fizemos no react) e passamos a URL, mas usamos
+// um Uri.parse pra transformar a string em uma URI.
+class API {
+  static Future getMovie(search) async {
+    var url = baseUrl + search;
+    return await http.get(Uri.parse(url));
+  }
+}
+
+// Criamos uma classe para representar os objetos que vão conter os filmes
+// e colocamos só os campos que vamos usar.
+class Movies {
+  late int id;
+  late String name;
+  late String link;
+  late String image;
+  Movies(int id, String name, String link, String image) {
+    this.id = id;
+    this.name = name;
+    this.link = link;
+    this.image = image;
+  }
+  Movies.fromJson(Map json)
+      : id = json['show']['id'],
+        name = json['show']['name'],
+        link = json['show']['url'],
+        image = json['show']['image']['medium'];
+}
